3a355386fb95e64963d93968f77a55aea6ac0c4c
cedricfonsat <fonsat.pro@gmail.com>
2024-09-18 08:08:36 +0200

    modif/ sept 2024

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,5 +22,33 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig',[
             'products' => $products
         ]);
+    }
+
+
+    #[Route('/user/promote/{id}', name: 'user_promote')]
+    public function promoteUser(int $id, EntityManagerInterface $entityManager): Response
+    {
+        // Récupère le repository de l'entité User
+        $userRepository = $entityManager->getRepository(User::class);
+
+        // Cherche l'utilisateur par ID
+        $user = $userRepository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        // Ajoute le rôle ROLE_ADMIN
+        $roles = $user->getRoles();
+        if (!in_array('ROLE_ADMIN', $roles)) {
+            $roles[] = 'ROLE_ADMIN';
+            $user->setRoles($roles);
+            
+            // Sauvegarde les modifications dans la base de données
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return new Response('User promoted to ROLE_ADMIN');
     }
 }
